ba8e0730e7c4c6211d520ad70b111acbbb596954
GhsVilela <ghsvilela@gmail.com>
2016-12-15 17:19:52 -0200

    GoogleMaps mostrando todos os tipos de mapa

--- app/src/main/java/com/party/party/MapsActivity.java
+++ app/src/main/java/com/party/party/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.party.party;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,55 +19,57 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     GoogleMap mGoogleMap;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(googleServicesAvaible()){
+        if (googleServicesAvaible()) {
             Toast.makeText(this, "Perfeito!!!!", Toast.LENGTH_LONG).show();
             setContentView(R.layout.activity_maps);
             initMap();
-        }
-        else
-        {
+        } else {
             //No Google Maps Layout
         }
     }
 
-    private void initMap(){
+    private void initMap() {
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
 
     }
 
-    public boolean googleServicesAvaible(){
+    public boolean googleServicesAvaible() {
 
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvaible = api.isGooglePlayServicesAvailable(this);
 
-        if(isAvaible == ConnectionResult.SUCCESS){
+        if (isAvaible == ConnectionResult.SUCCESS) {
             return true;
-        }
-        else if(api.isUserResolvableError(isAvaible))
-        {
+        } else if (api.isUserResolvableError(isAvaible)) {
             Dialog dialog = api.getErrorDialog(this, isAvaible, 0);
             dialog.show();
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Não foi possivel conectar com play services", Toast.LENGTH_LONG).show();
         }
         return false;
@@ -70,7 +78,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        goToLocationZoom(-19.01833, -49.480405, 15);
+        //goToLocationZoom(-19.01833, -49.480405, 15);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+        }
+        else
+        {
+            buildGoogleApiClient();
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     private void goToLocation(double lat, double lng) {
@@ -84,6 +122,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mGoogleMap.moveCamera(update);
     }
+
+    Marker marker;
 
     public void geoLocate(View view) throws IOException {
         EditText editText = (EditText) findViewById(R.id.editTextMaps);
@@ -101,6 +141,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         double lng = address.getLongitude();
         goToLocationZoom(lat, lng, 15);
 
+        setMarker(localizacao, lat, lng);
+
+
+    }
+
+    private void setMarker(String localizacao, double lat, double lng) {
+        if(marker != null){
+            marker.remove();
+        }
+
+        MarkerOptions options = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.iconepequeno))
+                .title(localizacao)
+                .position(new LatLng(lat, lng)).snippet("Party - IFTM");
+        marker = mGoogleMap.addMarker(options);
     }
 
     @Override
@@ -109,9 +164,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onCreateOptionsMenu(menu);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.mapTypeNone:
                 mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
                 break;
@@ -132,5 +187,52 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(location == null){
+            Toast.makeText(this, "Não foi possível pegar a localização", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
+            mGoogleMap.animateCamera(update);
+
+        }
+
     }
 }
